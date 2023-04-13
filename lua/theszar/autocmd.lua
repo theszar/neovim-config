53f06c6f4094af906850ced134653eb3e50ef4af
Tadeusz Szar <tadeusz.szar@gmail.com>
2023-04-13 08:06:11 +0200

    autocmd for restoring cursor position added

--- lua/theszar/autocmd.lua
+++ lua/theszar/autocmd.lua
@@ -1,7 +1,20 @@
--- highlight yanked text for 200ms using the "Visual" highlight group
-vim.cmd[[
-augroup highlight_yank
-autocmd!
-au TextYankPost * silent! lua vim.highlight.on_yank({higroup="Visual", timeout=200})
-augroup END
-]]
+vim.api.nvim_create_augroup('bufcheck', {clear = true})
+--
+-- highlight yanks
+vim.api.nvim_create_autocmd('TextYankPost', {
+    group    = 'bufcheck',
+    pattern  = '*',
+    callback = function() vim.highlight.on_yank{higroup="Visual", timeout=200} end })
+
+-- Return to last edit position when opening files
+vim.api.nvim_create_autocmd('BufWinEnter',  {
+    group    = 'bufcheck',
+    pattern  = '*',
+    callback = function()
+        if vim.fn.line("'\"") > 0 and vim.fn.line("'\"") <= vim.fn.line("$") then
+            vim.fn.setpos('.', vim.fn.getpos("'\""))
+            vim.cmd('normal zz') -- centering window works with BufWinEnter because it 
+                                 -- requires buffer to be displayed in the window so 
+                                 --with BufReadPost this will fail
+            vim.cmd('silent! foldopen')
+            end end })
